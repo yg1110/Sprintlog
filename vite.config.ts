82c7b3ac972ce9d7b1994785d6a3e513117d2350
yg1110 <younggil9488@gmail.com>
2026-03-08 14:56:53 +0900

    feat: locator js 추가

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            "@locator/babel-jsx/dist",
+            {
+              env: "development",
+            },
+          ],
+        ],
+      },
+    }),
+    tailwindcss(),
+  ],
 });
